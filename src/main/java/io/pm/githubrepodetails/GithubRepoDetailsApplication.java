e033b24e8174582ec4ca01d653527d953aebbaad
Piotr Melkowski <piotr.melkowski@gmail.com>
2019-01-06 21:41:43 +0100

    Initial commit.

---
+++ src/main/java/io/pm/githubrepodetails/GithubRepoDetailsApplication.java
@@ -0,0 +1,14 @@
+package io.pm.githubrepodetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepoDetailsApplication.class, args);
+	}
+
+}
+
